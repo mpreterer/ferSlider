b5713242c2ad880461fe55263abdbab7425a6684
Roland <mpreter@mail.ru>
2021-03-23 21:23:01 +0300

    Modificated double-range

--- range.js
+++ range.js
@@ -1,41 +1,56 @@
+const thumb = document.querySelector('.thumb');
 const thumbLeft = document.getElementById('thumbLeft');
+const thumbRight = document.getElementById('thumbRight');
 const field_range = document.querySelector('.field_range');
 const range = document.querySelector('.range');
 const containerRange = document.querySelector('.container_range');
-const thumb = document.querySelector('.thumb');
+
+
+// var View = {
+
+// }
 
 
 var module = {
-    eventRange: thumbLeft.onmousedown = function(event) {
+    eventThumbLeft: thumbLeft.onmousedown = function(event) {
         event.preventDefault();
+
         let shiftX = event.clientX - thumbLeft.getBoundingClientRect().left;
+
         document.addEventListener('mousemove', nowMouseMove);
         document.addEventListener('mousemove', changeWidth);
         document.addEventListener('mouseup', nowMouseUp);
+        
 
         function nowMouseMove(event) {
-            var newPos = event.clientX - shiftX - 34;
+            let newPos = event.clientX - shiftX - 34;
+            let rightEdge = range.offsetWidth - thumbLeft.offsetWidth;
             thumbLeft.style.left = newPos + 'px';
 
             if (newPos < 0) {
                 newPos = 0;
               }
-              let rightEdge = range.offsetWidth - thumbLeft.offsetWidth;
-              if (newPos > rightEdge) {
+
+            if (newPos > rightEdge) {
                 newPos = rightEdge;
               }
 
+
             thumbLeft.style.left = newPos + 'px';
+
+
            
         }
 
         function changeWidth() {
             let widthThumb = parseInt(thumb.style.width);
-            let x = parseInt(thumbLeft.style.left);
-            const MOVE = field_range.style.width;
-            field_range.style.width = x + widthThumb +'px';
-            console.log(field_range.style.width);
-            console.log(x)
+            // const MOVE = field_range.style.width;
+
+            var WIDTH = field_range.style.width;
+            var leftPositionThumbLeft = parseInt(thumbLeft.style.left);
+            var leftPositionThumbRight = parseInt(thumbRight.style.left);
+            field_range.style.left = leftPositionThumbLeft + 'px';
+            field_range.style.width = leftPositionThumbRight - leftPositionThumbLeft + widthThumb + 'px';
         }
 
         function nowMouseUp() {
@@ -43,5 +58,49 @@ var module = {
             document.removeEventListener('mousemove', nowMouseMove);
             document.removeEventListener('mousemove', changeWidth);
         }
-    }
+
+    },
+
+    eventThumbRight: thumbRight.onmousedown = function(event) {
+        event.preventDefault();
+
+        let shiftX = event.clientX - thumbRight.getBoundingClientRect().left;
+
+        document.addEventListener('mousemove', nowMouseMove);
+        document.addEventListener('mousemove', changeWidth);
+        document.addEventListener('mouseup', nowMouseUp);
+
+    
+        function nowMouseMove(event) {
+            var newPos = event.clientX - shiftX - 34;
+
+            if (newPos < 0) {
+                newPos = 0;
+              }
+              let rightEdge = range.offsetWidth - thumbRight.offsetWidth;
+              if (newPos > rightEdge) {
+                newPos = rightEdge;
+              }
+
+              thumbRight.style.left = newPos + 'px';
+           
+        }
+
+        function changeWidth() {
+            let widthThumb = parseInt(thumb.style.width);
+            let leftPositionThumbLeft = parseInt(thumbLeft.style.left);
+            let leftPositionThumbRight = parseInt(thumbRight.style.left);
+            const MOVE = field_range.style.width;
+
+            field_range.style.width = (leftPositionThumbRight-leftPositionThumbLeft) + widthThumb +'px';
+            field_range.style.left = leftPositionThumbLeft + 'px';
+            
+        }
+
+        function nowMouseUp() {
+            document.removeEventListener('mouseup' , nowMouseUp);
+            document.removeEventListener('mousemove', nowMouseMove);
+            document.removeEventListener('mousemove', changeWidth);
+        }
+    },
 }
