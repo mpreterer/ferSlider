132741f8b58058071a52faa8d53c78b55349f8e7
Roland <mpreter@mail.ru>
2021-03-23 11:48:24 +0300

    change values

--- range.js
+++ range.js
@@ -1,31 +1,35 @@
-var module = {
-    thumbLeft: document.getElementById('thumbLeft'),
-    field_range: document.querySelector('.field_range'),
-    slider: document.querySelector('.range'),
-    containerRange: document.querySelector('.container_range'),
+const thumbLeft = document.getElementById('thumbLeft');
+const field_range = document.querySelector('.field_range');
+const range = document.querySelector('.range');
+const containerRange = document.querySelector('.container_range');
 
+var module = {
 
     eventRange: thumbLeft.onmousedown = function(event) {
         event.preventDefault();
-
         let shiftX = event.clientX - thumbLeft.getBoundingClientRect().left;
         document.addEventListener('mousemove', nowMouseMove);
+        document.addEventListener('mousemove', changeWidth);
         document.addEventListener('mouseup', nowMouseUp);
 
         function nowMouseMove(event) {
             var newPos = event.clientX - shiftX - 34;
-            console.log(newPos);
             thumbLeft.style.left = newPos + 'px';
 
             if (newPos < 0) {
                 newPos = 0;
               }
-              let rightEdge = slider.offsetWidth - thumbLeft.offsetWidth;
+              let rightEdge = range.offsetWidth - thumbLeft.offsetWidth;
               if (newPos > rightEdge) {
                 newPos = rightEdge;
               }
 
             thumbLeft.style.left = newPos + 'px';
+           
+        }
+        function changeWidth() {
+            let x = parseInt(thumbLeft.style.left);
+            console.log(field_range);
         }
 
         function nowMouseUp() {
