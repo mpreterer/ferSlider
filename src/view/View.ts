cfd8f1bf0d76e0207739c5cd25c3438061a864a1
mpreter <dshabelyan@mail.ru>
2021-10-19 19:08:01 +0300

    continue write mvc

--- src/view/View.ts
+++ src/view/View.ts
@@ -1,3 +1,10 @@
-class View {
-    options: Options;
+import Observer from "../Observer/Observer";
+import { optionsParentDOM, valueOutput} from ""
+class View extends Observer {
+    constructor(parentDOM: optionsParentDOM, modelOptions: valueOutput) {
+        super();
+
+        this.modelOptions = modelOptions;
+
+    }
 }
