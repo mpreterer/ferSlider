cb962de5efe3a3278eebbc91a2d6d1ad475b3da2
mpreter <dshabelyan@mail.ru>
2021-10-20 22:39:55 +0300

    Added Interface Settings

---
+++ src/model/InterfaceSliderSettings.ts
@@ -0,0 +1,13 @@
+interface InterfaceSliderSettings {
+    from?: number;
+    to?: number;
+    min?: number;
+    max?: number;
+    step?: number;
+    isRange?: boolean;
+    isVertical?: boolean;
+    hasTip?: boolean;
+    hasLine?: boolean;
+}
+
+export default InterfaceSliderSettings;
