0f089566f3558ab7357fc9735aecce92f3a6236b
mpreter <dshabelyan@mail.ru>
2021-11-08 16:12:05 +0300

    transfered files

--- src/interfaces/IJQuery.ts
+++
@@ -1,9 +0,0 @@
-import IModelOptions from './IModelOptions';
-import ferSlider from './ferSlider';
-
-
-declare global {
-    interface JQuery {
-        ferSlider(options: IModelOptions): ferSlider;
-    }
-}
--- src/interfaces/ferSlider.ts
+++
@@ -1,5 +0,0 @@
-class ferSlider {
-
-}
-
-export default ferSlider;
