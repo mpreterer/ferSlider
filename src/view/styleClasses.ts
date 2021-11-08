ecfb457bd6b915996624266b2ebd03b96a8fa3d1
mpreter <dshabelyan@mail.ru>
2021-11-08 15:06:22 +0300

    Added View and style elements for slider

---
+++ src/view/styleClasses.ts
@@ -0,0 +1,9 @@
+const styleClasses = {
+    SLIDER: 'j-fer-slider',
+    THUMB: 'j-fer-slider__thumb',
+    BAR: 'j-fer-slider__bar',
+    TIP: 'j-fer-slider__tip',
+    VERTICAL: 'j-fer-slider_vertical',
+  };
+  
+  export default styleClasses;
