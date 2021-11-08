9186da2cf718488a9738e3c8a75b3b73d7cb2b8c
mpreter <dshabelyan@mail.ru>
2021-11-08 12:26:32 +0300

    Added Iterfaces from Model

---
+++ src/interfaces/IModelOptions.ts
@@ -0,0 +1,13 @@
+interface IModelOptions {
+    minValue: number,
+    maxValue: number,
+    step: number,
+    valueFrom: number,
+    valueTo: number,
+    isRange: false,
+    isTip: true,
+    isBar: true,
+    isVertical: false,
+  }
+  
+export default IModelOptions 
