4ff74104f1197087dd744b7bc5b74000d1c8a2cf
mpreter <dshabelyan@mail.ru>
2021-11-08 15:52:43 +0300

    test slider

---
+++ src/ferSlider.ts
@@ -0,0 +1,21 @@
+import IModelOptions from "./interfaces/IModelOptions";
+import Presenter from "./Presenter/Presenter";
+
+class ferSlider {
+
+  private options: IModelOptions;
+  private domParent: HTMLDivElement;
+
+  constructor (domParent: HTMLDivElement, options: IModelOptions) {
+    this.domParent = domParent
+    this.options = options;
+    this.init();
+  }
+
+  private init() {
+    const presenter = new Presenter(this.domParent, this.options)
+  }
+
+}
+
+export default ferSlider; 
