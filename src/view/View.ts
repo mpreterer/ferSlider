ddf385598cb4ed1a5f46f0c2e471779eba63306b
mpreter <dshabelyan@mail.ru>
2021-11-08 15:06:54 +0300

    Added class View

--- src/view/View.ts
+++ src/view/View.ts
@@ -1,36 +1,39 @@
 // import Observer from "../Observer/Observer";
 // import { optionsParentDOM, updateOutputValue, valueOutput} from "../utils/types/namespace.ts";
+import Observer from "../Observer/Observer";
 
-class View {
-    options: Options;
-    $el: HTMLElement;
+class View extends Observer{
+    // options: Options;
+    // $el: HTMLElement;
   
-    SVPoint: SVPoint;
-    SVRange: SVRange;
-    SVLine: SVLine;
+    // SVPoint: SVPoint;
+    // SVRange: SVRange;
+    // SVLine: SVLine;
   
-    setOptions(options: Options) {
-      this.options = options
-      console.log('Options was set');
-    }
+    // setOptions(options: Options) {
+    //   this.options = options
+    //   console.log('Options was set');
+    // }
   
-    initProps() {
-      this.$el = document.getElementById(this.options.id);
-      console.log('Properties init complete!');
-    }
+    // initProps() {
+    //   this.$el = document.getElementById(this.options.id);
+    //   console.log('Properties init complete!');
+    // }
   
-    render(template) {
-      this.$el.innerHTML = template
-      console.log('Template uploaded');
-    }
+    // render(template) {
+    //   this.$el.innerHTML = template
+    //   console.log('Template uploaded');
+    // }
   
-    initComponents() {
-      this.SVPoint = new SVPoint(this);
-      this.SVRange = new SVRange(this);
+    // initComponents() {
+    //   this.SVPoint = new SVPoint(this);
+    //   this.SVRange = new SVRange(this);
   
-      this.SVPoint.start();
-      this.SVRange.start();
+    //   this.SVPoint.start();
+    //   this.SVRange.start();
   
-      console.log('init complete!');
-    }
+    //   console.log('init complete!');
+    // }
   }
+
+  export default View;
