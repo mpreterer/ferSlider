2dd1538243c35b8e2d2eee1e143be5f4fe540998
mpreter <dshabelyan@mail.ru>
2021-11-08 13:42:19 +0300

    Added Model

---
+++ src/model/defaultSettings.ts
@@ -0,0 +1,15 @@
+import IModelOptions from '../interfaces/IModelOptions';
+
+const defaultSettings: IModelOptions = {
+    minValue: 0,
+    maxValue: 100,
+    step: 1,
+    valueFrom: 50,
+    valueTo: 100,
+    isRange: false,
+    isTip: true,
+    isBar: true,
+    isVertical: false,
+};
+
+export default defaultSettings;
