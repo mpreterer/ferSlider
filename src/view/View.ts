e2c051ae9dc9831dfc80dec7e16643f4cb2d3106
mpreter <dshabelyan@mail.ru>
2021-11-18 19:49:50 +0300

    Create test View template

--- src/view/View.ts
+++ src/view/View.ts
@@ -1,38 +1,58 @@
 // import { optionsParentDOM, updateOutputValue, valueOutput} from "../utils/types/namespace.ts";
 import Observer from "../Observer/Observer";
+import IModelOptions from "../interfaces/IModelOptions";
+import styleClasses from "./styleClasses";
 
-class View extends Observer{
-    private options: Options;
-    // $el: HTMLElement;
-  
-    // SVPoint: SVPoint;
-    // SVRange: SVRange;
-    // SVLine: SVLine;
-  
-    // setOptions(options: Options) {
-    //   this.options = options
-    //   console.log('Options was set');
-    // }
-  
-    // initProps() {
-    //   this.$el = document.getElementById(this.options.id);
-    //   console.log('Properties init complete!');
-    // }
-  
-    // render(template) {
-    //   this.$el.innerHTML = template
-    //   console.log('Template uploaded');
-    // }
-  
-    // initComponents() {
-    //   this.SVPoint = new SVPoint(this);
-    //   this.SVRange = new SVRange(this);
-  
-    //   this.SVPoint.start();
-    //   this.SVRange.start();
-  
-    //   console.log('init complete!');
-    // }
+import Thumb from "./components/thumb/thumb";
+import Bar from "./components/bar/bar";
+import Step from "./components/step/step";
+import Tip from "./components/Tip/tip";
+
+
+class View extends Observer {
+  private modelOptions: IModelOptions;
+  private domParent: HTMLDivElement;
+
+  private bar: Bar;
+  private thumb: Thumb;
+  private step: Step;
+  private tip: Tip;
+
+  constructor (domParent: HTMLDivElement, modelOptions: IModelOptions) {
+    super();
+
+    this.domParent = domParent;
+    this.modelOptions = modelOptions;
+    this.render();
+  }
+
+  public updateModelOptions(newModelOptions: IModelOptions) {
+    this.modelOptions = newModelOptions;
+  }
+
+  private render () {
+    this.initViewComponents();
+    this.createSlider();
+  }
+
+  private initViewComponents () {
+    
+    this.bar = new Bar();
+    this.thumb = new Thumb();
+
+  }
+
+  private createSlider () {
+    const templateSlider = document.createElement('div');
+    templateSlider.classList.add(`${styleClasses.SLIDER}`);
+
+    const tplBar = templateSlider.appendChild(this.bar.getHtml());
+    const tplThumb = templateSlider.appendChild(this.thumb.getHtml());
+
+    tplThumb.appendChild(this.thumb.getHtml());
+
+    this.domParent.appendChild(templateSlider);
+  }
   }
 
   export default View;
--- src/view/styleClasses.ts
+++ src/view/styleClasses.ts
@@ -1,6 +1,8 @@
 const styleClasses = {
     SLIDER: 'j-fer-slider',
     THUMB: 'j-fer-slider__thumb',
+    STEP: 'j-fer-slider__step',
+    STEP_ITEM: 'j-fer-slider__step-item',
     BAR: 'j-fer-slider__bar',
     TIP: 'j-fer-slider__tip',
     VERTICAL: 'j-fer-slider_vertical',
