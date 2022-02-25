5facd82d97baf1d3d72855cdeb6dd550327cb34d
mpreter <dshabelyan@mail.ru>
2022-02-25 14:15:18 +0300

    Coccret class preview

--- src/demo/index.ts
+++ src/demo/index.ts
@@ -6,42 +6,45 @@ import './panel/index';
 import './panel/panel.scss';
 
 const sliderContainer1 = {
-  containerSlider: $('.js-slider__container-slider-1'),
-  containerPanel: $('.js-slider__container-panel-1')
+  containerSlider: $('.js-container__slider-1'),
+  containerPanel: $('.js-container__panel-1')
 };
 
-// const sliderContainer2 = {
-//   containerSlider: $('.js-slider__container-slider-2'),
-//   containerPanel: $('.js-slider__container-panel-2')  
-// };
+const sliderContainer2 = {
+  containerSlider: $('.js-container__slider-2'),
+  containerPanel: $('.js-container__panel-2')  
+};
 
-// const sliderContainer3 = {
-//   containerSlider: $('.js-slider__container-slider-2'),
-//   containerPanel: $('.js-slider__container-panel-3')
-// };
-
-// const slider_1 = sliderContainer1.containerSlider.ferSlider({});
-// const slider_2 = sliderContainer2.containerSlider.ferSlider({
-//   minValue: -10,
-//   maxValue: 0,
-//   isVertical: true,
-//   step: 0.01,
-//   isRange: false,
-//   valueFrom: -7
-// });
-// const slider_3 = sliderContainer3.containerSlider.ferSlider({
-//   minValue: -42,
-//   maxValue: 42,
-//   isRange: true,
-//   valueFrom: {
-//     minValue: -32,
-//     maxValue: 32
-//   },
-//   step: 5,
-//   isTip: true,
-//   isStep: true,
-// });
+const sliderContainer3 = {
+  containerSlider: $('.js-container__slider-3'),
+  containerPanel: $('.js-container__panel-3')
+};
 
 const slider_1 = sliderContainer1.containerSlider.ferSlider({});
+const slider_2 = sliderContainer2.containerSlider.ferSlider({
+  minValue: -42,
+  maxValue: 42,
+  isRange: true,
+  valueFrom: {
+    minValue: -32,
+    maxValue: 32
+  },
+  step: 5,
+  isTip: true,
+  isStep: true,
+});
+const slider_3 = sliderContainer3.containerSlider.ferSlider({
+  minValue: -10,
+  maxValue: 0,
+  isVertical: true,
+  step: 0.01,
+  isRange: false,
+  valueFrom: -7
+});
+
+
 sliderContainer1.containerPanel.demoPanel(slider_1);
+sliderContainer2.containerPanel.demoPanel(slider_2);
+sliderContainer3.containerPanel.demoPanel(slider_3);
+
 
