09bde96ebfe14058493da72930f32eda4b221bf3
David <82500381+mpreterer@users.noreply.github.com>
2022-10-12 16:29:50 +0300

    Delete src/demo directory

--- src/demo/panel/utils/typings/globals/IJQueryPanel.d.ts
+++
@@ -1,8 +0,0 @@
-import IModelSettings from "../../../../../range/interfaces/IModelSettings";
-import DemoPanel from "../../../DemoPanel";
-
-declare global {
-  interface JQuery {
-    DemoPanel(settings?: IModelSettings): DemoPanel;
-  }
-}
