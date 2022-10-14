4c24279986eb5f09bb7f210584c2a87b728a99a3
David <82500381+mpreterer@users.noreply.github.com>
2022-10-14 10:32:14 +0300

    Update Model.spec.ts

--- src/Model/Model.spec.ts
+++ src/Model/Model.spec.ts
@@ -1,11 +1,18 @@
 import IValidSettings from '../interfaces/IValidSettings';
-import { TUpdateThumb } from '../interfaces/types';
+import { TUpdateThumb, UpdateValues } from '../interfaces/types';
 import defaultSettings from './defaultSettings';
 import Model from './Model';
 
 describe("Model:", () => {
-  const settings = defaultSettings;
-  const model = new Model(settings);
+  let model: Model;
+
+  beforeEach(() => {
+    model = new Model(defaultSettings);
+  });
+
+  test('Возвращает экземпляр класса', () => {
+    expect(model).toBeInstanceOf(Model);
+  });
 
   describe("updateModelSettings:", () => {
     test("Обновляются настройки слайдера на вводимые", () => {
@@ -43,9 +50,38 @@ describe("Model:", () => {
       expect(model.settings.valueFrom).toEqual(valueTo);
       expect(model.settings.valueTo).toEqual(valueTo);
     });
+
+    test('Не будет принимать не валидные значения', () => {
+      const newSettings = {
+        ...defaultSettings,
+        ...{
+          maxValue: NaN,
+          minValue: NaN,
+          step: NaN,
+          valueFrom: NaN,
+          valueTo: NaN,
+          isRange: true,
+        },
+      }
+
+      model.updateModelSettings(newSettings);
+      expect(model.settings.maxValue).toBe(defaultSettings.maxValue);
+      expect(model.settings.maxValue).toBe(defaultSettings.maxValue);
+      expect(model.settings.valueFrom).toBe(defaultSettings.valueFrom);
+      expect(model.settings.step).toBe(defaultSettings.step);
+      expect(model.settings.valueTo).toBe(defaultSettings.valueTo);
+    });
   });
 
   describe("updateCurrentValueSettings:", () => {
+    test('Не будет принимать не валидные значения', () => {
+      const thumb: TUpdateThumb = { handle: "thumbLeft", value: NaN };
+
+      model.updateCurrentValueSettings(thumb)
+
+      expect(model.settings.valueFrom).toEqual(defaultSettings.valueFrom);
+    });
+
     test("Обновление valueFrom на проверенное", () => {
       const thumb: TUpdateThumb = { handle: "thumbLeft", value: 10 };
 
@@ -53,6 +89,25 @@ describe("Model:", () => {
       const UpdateValueSettings = model.settings;
 
       expect(UpdateValueSettings.valueFrom).toBe(10);
+    });
+
+    test('должен обновлять текущее значение слайдера', () => {
+      const newFrom:UpdateValues = { handle: 'thumbLeft', value: 23 };
+      const newTo:UpdateValues = { handle: 'thumbRight', value: 25 };
+      const newSettings: IValidSettings = {
+        ...defaultSettings,
+        ...{
+          isRange: true,
+        },
+      };
+
+      model.updateModelSettings(newSettings);
+
+      model.updateCurrentValueSettings(newFrom);
+      model.updateCurrentValueSettings(newTo);
+
+      expect(model.settings.valueFrom).toEqual(newFrom.value);
+      expect(model.settings.valueTo).toEqual(newTo.value);
     });
 
     test(`Оповещение наблюдателей 
@@ -129,6 +184,27 @@ describe("Model:", () => {
       };
 
       expect(Model.getDiapason(value, min, max)).toEqual(value);
+    });
+  });
+
+  describe('getDiapason:', () => {
+    test('getModelSettings вернет дефолтную настройку', () => {
+      expect(model.getModelSettings()).toBe(defaultSettings);
+    });
+
+    test('getModelSettings новые настройки', () => {
+      const newSettings = {
+        ...defaultSettings,
+        ...{
+          isRange: true,
+          valueTo: 123,
+          valueFrom: 10,
+          isTip: false,
+        },
+      }
+      model.updateModelSettings(newSettings);
+
+      expect(model.getModelSettings()).toBe(newSettings);
     });
   });
 });
