423d83d684f9b7daad8763b68075965e2b065cd4
mpreter <dshabelyan@mail.ru>
2022-12-19 21:15:18 +0300

    Transfered FerSlider #9

---
+++ src/slider/FerSlider.ts
@@ -0,0 +1,34 @@
+import Controller from './Controller/Controller';
+import IModelSettings from './interfaces/IModelSettings';
+import IValidSettings from './interfaces/IValidSettings';
+import { SliderEvents, TDOMParents, TUpdateThumb } from './interfaces/types';
+
+class FerSlider {
+  constructor (domParent: TDOMParents, settings: IModelSettings) {
+    this.controller = new Controller(domParent, settings as IValidSettings);
+  }
+
+  public updateValues (thumb: TUpdateThumb) {
+    this.controller.updateValues(thumb);
+  }
+
+  public updateSettings (settings: IModelSettings): void {
+    this.controller.updateSettings(settings);
+  }
+
+  public subscribe (event: SliderEvents, fun: Function): void {
+    this.controller.subscribe(event, fun);
+  }
+
+  public unsubscribe (event: SliderEvents, fun: Function): void {
+    this.controller.unsubscribe(event, fun);
+  }
+
+  private controller: Controller;
+
+  get settings (): IValidSettings {
+    return this.controller.settings;
+  }
+}
+
+export default FerSlider;
